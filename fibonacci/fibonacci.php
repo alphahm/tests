58a0156f10119d697ee8e87154f0162f5048882f
alphahm <ahmwebdev@gmail.com>
2018-12-31 11:51:40 +0000

    Create fibonacci.php

---
+++ fibonacci/fibonacci.php
@@ -0,0 +1,13 @@
+<?php
+
+// Fibonacci recursion test
+function fibonacci($n)
+{
+    if ($n == 1 || $n == 0) {
+        return $n;
+    }
+
+    return fibonacci($n - 1) + fibonacci($n - 2);
+}
+
+echo fibonacci(40);
